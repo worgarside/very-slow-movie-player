4a8e49c31136ab338ff69c674729b0c54a19d372
worgarside-dev <worgarside.dev@gmail.com>
2023-09-13 18:41:33 +0100

    Update project to run from local venv (#130)
    * Update project to run from local venv
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: Will Garside <worgarside@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- service/install.sh
+++ service/install.sh
@@ -3,6 +3,6 @@
 systemctl stop vsmp.service || :
 cp vsmp.service /etc/systemd/system/
 echo "Service file copied to /etc/systemd/system/vsmp.service"
-systemctl disnable vsmp.service
+systemctl disable vsmp.service
 systemctl enable vsmp.service
 systemctl start vsmp.service
